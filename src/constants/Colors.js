7e635e84453fbb5aea962033cd865f6e37b976ba
Timothy Adamson <MrTimcakes@gmail.com>
2020-04-29 13:28:00 +0100

    Update Colors.js

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,14 +1,9 @@
-const tintColor = '#2f95dc';
-
 export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
+  color1: 'rgba(35, 46, 209 , 1)',
+  color2: 'rgba(86, 99, 255, 1)',
+  color3: 'rgba(110, 127, 170, 1)',
+  color4: 'rgba(255, 120, 0, 1)',
+  color5: 'rgba(34, 34, 34, 1)',
+
+  offWhite: '#fAfAfA',
 };
