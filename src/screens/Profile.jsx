433851c26df32970dd1fa93ef3f60630e1a64d37
Timothy Adamson <MrTimcakes@gmail.com>
2020-04-29 13:26:32 +0100

    Placeholder screens

---
+++ src/screens/Profile.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { 
+  ScrollView, 
+  StyleSheet, 
+  Text,
+  View,
+  Button,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+  ImageBackground,
+  FlatList,
+  TextInput,
+  Switch,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { withFirebaseHOC } from 'kitty/utilities/Firebase'
+
+function Settings({firebase}){
+  return (
+    <SafeAreaView>
+      
+      <Text>Kitty Profile</Text>
+
+    </SafeAreaView>
+  );
+}
+
+export default withFirebaseHOC(Settings)
